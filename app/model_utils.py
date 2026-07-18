6651c37fe362bf87bc69c824a4df5a1c4019430f
Andres Fresnedo <andfresnedo@gmail.com>
2026-07-18 12:21:58 -0700

    add comment explaining function

--- app/model_utils.py
+++ app/model_utils.py
@@ -3,6 +3,10 @@ from datetime import UTC, datetime
 from sqlalchemy import ColumnElement, column, or_
 
 
+# This function allows us to avoid freezing the timestamp when used as a mapped_column
+# default in SQLAlchemy models. It'll be called once per row insertion, rather than
+# once at import time, so each row gets a unique timestamp instead of all rows sharing
+# the same one.
 def utcnow() -> datetime:
     return datetime.now(UTC)
 
